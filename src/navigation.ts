0e7960c2fdac906492a574de58b0e3a2acd381be
mrevanzak <mrevanzak@gmail.com>
2025-01-07 18:10:31 +0700

    feat: footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -70,48 +70,20 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
+      text: 'Services',
+      href: '#',
     },
     {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
+      text: 'Projects',
+      href: '#',
     },
     {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
+      text: 'Contact Us',
+      href: '#',
     },
     {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
+      text: 'Get Company Profile',
+      href: getAsset('/company-profile.pdf'),
     },
   ],
   secondaryLinks: [
@@ -119,11 +91,24 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram-filled', href: 'https://www.instagram.com/samarta_id' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin-filled', href: 'https://www.linkedin.com' },
+    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  contacts: [
+    {
+      icon: 'tabler:map-pin-filled',
+      text: 'Kalijudan Regency 10, Surabaya',
+    },
+    {
+      icon: 'tabler:phone-filled',
+      text: '+62 813-3067-6881',
+    },
+    {
+      icon: 'tabler:mail-filled',
+      text: 'contact@samarta.co',
+      href: 'mailto:contact@samarta.co',
+    },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
